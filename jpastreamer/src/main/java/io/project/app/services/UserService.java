76c927dfdf3ee5b0f8479e0b6280238b9eff48b2
Armen Arzumanyan <armena@YLT160001.corp.optym.net>
2020-09-15 22:36:17 +0400

    added spring integration and it works

--- jpastreamer/src/main/java/io/project/app/services/UserService.java
+++ jpastreamer/src/main/java/io/project/app/services/UserService.java
@@ -5,8 +5,14 @@
  */
 package io.project.app.services;
 
+import com.github.javafaker.Faker;
 import com.speedment.jpastreamer.application.JPAStreamer;
+import io.project.app.domain.User;
+import io.project.app.domain.User$;
+import java.util.Date;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,13 +24,71 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserService {
-    
+
     @Autowired
     private EntityManager em;
-    
-    
-    public void init(){
+
+    private final JPAStreamer jpaStreamer;
+
+    public UserService(JPAStreamer jpaStreamer) {
+        this.jpaStreamer = jpaStreamer;
+    }
+
+    public void initData() {
+
+        Faker faker = new Faker();
+        User user = null;
+        for (int i = 0; i < 50; i++) {
+            faker = new Faker();
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            user = new User();
+            user.setEmail(firstName + "." + lastName + "@gmail.com");
+            user.setFirstname(firstName);
+            user.setLastname(lastName);
+            user.setZipcode(faker.address().zipCode());
+            user.setMobileno("+101 " + System.currentTimeMillis());
+            user.setStatus(i);
+            user.setRegisterDate(new Date(System.currentTimeMillis()));
+            em.persist(user);
+            em.flush();
+
+        }
+
+        for (int i = 0; i < 25; i++) {
+            faker = new Faker();
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            user = new User();
+            user.setEmail(firstName + "." + lastName + "@yahoo.com");
+            user.setFirstname(firstName);
+            user.setLastname(lastName);
+            user.setZipcode(faker.address().zipCode());
+            user.setMobileno("+200 " + System.currentTimeMillis());
+            user.setStatus(i);
+            user.setRegisterDate(new Date(System.currentTimeMillis()));
+            em.persist(user);
+            em.flush();
+
+        }
 
     }
     
+    
+    public void read(){
+//           EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("streamer");
+//
+//        JPAStreamer jpaStreamer = JPAStreamer.createJPAStreamerBuilder("streamer")
+//                .build();
+        jpaStreamer.stream(User.class) // Film.class is the @Entity representing the film-table
+                .filter(User$.email.contains("gmail"))
+               //// .sorted(User$.status.reversed().thenComparing(User$.email.comparator()))
+                .skip(1)
+                .limit(50)
+                .forEach(System.out::println);
+
+       /// entityManagerFactory.close();
+
+    }
+
 }
--- jpastreamer/src/main/java/io/project/app/streamer/jpastreamer/JpaStreamerApplication.java
+++ jpastreamer/src/main/java/io/project/app/streamer/jpastreamer/JpaStreamerApplication.java
@@ -1,13 +1,6 @@
 package io.project.app.streamer.jpastreamer;
 
-import com.github.javafaker.Faker;
-import com.speedment.jpastreamer.application.JPAStreamer;
-import io.project.app.domain.User;
-import io.project.app.domain.User$;
-import java.util.Date;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import io.project.app.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -34,10 +26,8 @@ import org.springframework.web.filter.CorsFilter;
 @EntityScan("io.project.app.domain")
 @EnableAsync
 @Slf4j
-public class JpaStreamerApplication {
-
-    @Autowired
-    private EntityManager em;
+public class JpaStreamerApplication {    
+    
 
     public static void main(String[] args) {
         final SpringApplication application = new SpringApplication(JpaStreamerApplication.class);
@@ -51,62 +41,18 @@ public class JpaStreamerApplication {
         return new RestTemplate();
     }
 
+    @Autowired
+    private UserService userService;
+    
     @EventListener(ApplicationReadyEvent.class)
-    @Async
     @Transactional
     public void init() {
         log.info("You can once run this for db migration");
-        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/globus", "postgres", "globus2021").load();
-        flyway.clean();
-        flyway.migrate();
-        Faker faker = new Faker();
-        User user = null;
-        for (int i = 0; i < 50; i++) {
-            faker = new Faker();
-            String firstName = faker.name().firstName();
-            String lastName = faker.name().lastName();
-            user = new User();
-            user.setEmail(firstName + "." + lastName + ".@gmail.com");
-            user.setFirstname(firstName);
-            user.setLastname(lastName);
-            user.setZipcode(faker.address().zipCode());
-            user.setMobileno("+101 " + System.currentTimeMillis());
-            user.setStatus(i);
-            user.setRegisterDate(new Date(System.currentTimeMillis()));
-            em.persist(user);
-            em.flush();
-
-        }
-
-        for (int i = 0; i < 25; i++) {
-            faker = new Faker();
-            String firstName = faker.name().firstName();
-            String lastName = faker.name().lastName();
-            user = new User();
-            user.setEmail(firstName + "." + lastName + ".@yahoo.com");
-            user.setFirstname(firstName);
-            user.setLastname(lastName);
-            user.setZipcode(faker.address().zipCode());
-            user.setMobileno("+200 " + System.currentTimeMillis());
-            user.setStatus(i);
-            user.setRegisterDate(new Date(System.currentTimeMillis()));
-            em.persist(user);
-            em.flush();
-
-        }
-
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("streamer");
-
-        JPAStreamer jpaStreamer = JPAStreamer.createJPAStreamerBuilder("streamer")
-                .build();
-        jpaStreamer.stream(User.class) // Film.class is the @Entity representing the film-table
-                .filter(User$.email.contains("gmail.com"))
-                .sorted(User$.status.reversed().thenComparing(User$.email.comparator()))
-                .skip(3)
-                .limit(50)
-                .forEach(System.out::println);
-
-        entityManagerFactory.close();
+//        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/globus", "postgres", "globus2021").load();
+//        flyway.clean();
+//        flyway.migrate();
+        //userService.initData();
+        userService.read();
 
     }
 
